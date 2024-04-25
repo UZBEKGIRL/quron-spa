372352754f38454f871afe5cbb6de4e85a6b7538
UZBEKGIRL <rfarangiz1305@gmail.com>
2024-04-25 19:45:15 +0500

    commit 3

--- main.js
+++ main.js
@@ -2,5 +2,8 @@ let app = document.querySelector(".app");
 
 let h1 = document.createElement("h1");
 h1.textContent = "Hi";
+let p = document.createElement("p");
+p.textContent = "lorem";
 
 app.appendChild(h1);
+app.append(p);
